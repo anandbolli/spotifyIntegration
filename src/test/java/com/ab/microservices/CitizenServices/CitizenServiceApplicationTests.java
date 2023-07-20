b404c7cabdf91633350355dc4cf956784655f570
anandbolli <anandbolli@gmail.com>
2023-07-21 00:42:27 +0530

    test
    initial commit

---
+++ src/test/java/com/ab/microservices/CitizenServices/CitizenServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ab.microservices.CitizenServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitizenServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
